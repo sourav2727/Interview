481587a3f07b8f1a9ba10d550c1b6214a353be13
sourav jana <souravjana9641@gmail.com>
2024-07-26 12:12:20 +0530

    Implicit conversation in JS

---
+++ number.js
@@ -0,0 +1,4 @@
+let a = "122" - 2
+console.log(a)
+//JavaScript implicitly converts the string to a number before performing the subtraction. This process is known as type coercion.
+// Type coercion in JavaScript refers to the automatic or implicit conversion of values from one data type to another.This happens automatically when operators are applied between different types.
