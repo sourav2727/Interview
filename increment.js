d2f3a4e304a61bdd9b212a807242ec1ff52b36c6
sourav jana <souravjana9641@gmail.com>
2024-07-26 12:09:34 +0530

    Increment and Decrement

---
+++ increment.js
@@ -0,0 +1,6 @@
+var a =5
+console.log(a++)//post increament gives previous value which is 5
+console.log(a)//updated value 6
+console.log(++a)//gives updated value 7 [a=a+1-------->a=6+1]
+console.log(a--)
+console.log(a)
